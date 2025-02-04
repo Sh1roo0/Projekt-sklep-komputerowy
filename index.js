b2079aa6522831fea788138b71a0b1ff908c2507
a <a>
2025-02-04 09:01:32 +0100

    fixed website 3

--- index.js
+++ index.js
@@ -5,36 +5,36 @@ let intervalID = null;
 //initializeSlider();
 document.addEventListener("DOMContentLoaded", initializeSlider);
 
-function initializeSlider(){
+function initializeSlider() {
 
-    if(slides.length > 0){
+    if (slides.length > 0) {
 
         slides[slideIndex].classList.add("displaySlide");
         intervalID = setInterval(nextSlide, 5000);
     }
-   
-    
-}
-function showSlide(index){
 
-if(index >= slides.length){
-    slideIndex = 0;
-}
-else if(index < 0){
-    slideIndex = slides.length - 1;
-}
 
-slides.forEach(slide =>{
-    slide.classList.remove("displaySlide");
-});
-slides[slideIndex].classList.add("displaySlide");
 }
-function prevSlide(){
-clearInterval(intervalID);
-slideIndex--;
-showSlide(slideIndex);
+function showSlide(index) {
+
+    if (index >= slides.length) {
+        slideIndex = 0;
+    }
+    else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
 }
-function nextSlide(){
+function prevSlide() {
+    clearInterval(intervalID);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+function nextSlide() {
     slideIndex++;
     showSlide(slideIndex);
 }
@@ -42,36 +42,24 @@ function nextSlide(){
 
 
 
-function Walidacja(){
+function Walidacja() {
     let newsletter = document.getElementById("newsletter").value;
 
 
-    if(newsletter == ""){
-    alert("Niepodano adresu e-mail");
-    return;
-}else{
-    alert("Dziękujemy za zapisanie się do naszego newslettera!");
-}
-
-
-
-document.getElementById("newsletter").value = "";
-}
-
-
-
-var icon = document.getElementById("icon");
-
-  icon.onclick = function(){
-    document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        icon.src = "img/techpoint.png";
-
-    }else{
-        icon.src = "img/techpoint.png";
+    if (newsletter == "") {
+        alert("Niepodano adresu e-mail");
+        return;
+    } else {
+        alert("Dziękujemy za zapisanie się do naszego newslettera!");
     }
-  }
-  function Modes() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+
+    document.getElementById("newsletter").value = "";
+}
+
+const loader = document.getElementById("loader");
+const container = document.getElementById("container");
+
+window.addEventListener("load", () => {
+    loader.style.display = "none";
+    container.style.display = "block";
+})
